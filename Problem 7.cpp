41ebef2f87e4cc6e2bd98516bf08ad616cac369a
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2023-10-07 23:02:19 +0530

    Reverse Integer

---
+++ Problem 7.cpp
@@ -0,0 +1,17 @@
+/* Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0. */
+
+class Solution {
+public:
+    int reverse(int x) {
+        int n=x,c=0,rev=0;
+    while(x!=0)
+    {
+        int r=x%10;
+        if(rev>INT_MAX/10 || rev<INT_MIN/10)
+        return 0;
+        rev=(rev*10)+r;
+        x/=10;
+    } 
+    return rev;
+    }
+};
