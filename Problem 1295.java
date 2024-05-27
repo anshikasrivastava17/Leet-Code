d4018f1954e9d7aaaab34d3d9fe59eb5164dfdd7
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-05-27 23:35:32 +0530

    Update Problem 1295.java

--- Problem 1295.java
+++ Problem 1295.java
@@ -1,6 +1,7 @@
 /* Given an array nums of integers, return how many of them contain an even number of digits. */
 
- 
+ //Method 1 - count by conventional while loop method
+//Method 2 - use Math.log to count number of digits
 
 class Solution {
     public int findNumbers(int[] nums) {
@@ -13,7 +14,7 @@ class Solution {
         return c;
     }
 
-    static int digits(int n){
+    static int digits(int n){  //M-2 instead of counting like this use 'return (int)(Math.log10(n)) + 1; '  in place of whole function
         int d=0;
         while(n!=0){
             d++;
