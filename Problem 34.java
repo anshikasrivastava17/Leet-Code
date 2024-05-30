a124c30639c891f9f8cf1bd8e325381cf32b13a7
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-05-30 16:45:59 +0530

    Find 1st & last position of element in sorted array

--- Problem 34.java
+++ Problem 34.java
@@ -4,7 +4,7 @@ You must write an algorithm with O(log n) runtime complexity. */
 
 
 //given below is with O(n) complexity
-class Solution {
+/* class Solution {
     public int[] searchRange(int[] arr, int target) {
         int c = 0;
         int nums[]=new int[2];
@@ -29,5 +29,41 @@ class Solution {
      return nums;
     }
     }
+}   */
 
+
+class Solution {
+    public int[] searchRange(int[] arr, int target) {
+       int []ans = {-1,-1};
+       int start = search(arr,target,true);
+       int end = search(arr,target,false);
+       ans[0]=start;
+       ans[1]=end;
+       return ans;
+    }
+
+
+    int search(int[] arr, int target, boolean first_index) {
+        int start = 0;
+        int end = arr.length - 1;
+        int ans = -1;
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {   //we come in here only when target==arr[mid] ie. whenever target is found
+                // potential ans found
+                ans = mid;
+                if(first_index)  //left subarray
+                end = mid - 1;
+                else //last occurence -> right subarray
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
 }
