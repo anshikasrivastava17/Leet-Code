a4207fdb606a648233801f04fbe77958733651c3
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-06-02 16:41:44 +0530

    XOR Operation in Array

--- Arrays/Problem 1486.java
+++ Arrays/Problem 1486.java
@@ -1,7 +1,6 @@
 /* You are given an integer n and an integer start.
 Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
 Return the bitwise XOR of all elements of nums. */
-
 class Solution {
     public int xorOperation(int n, int start) {
         int [] nums = new int [n];
