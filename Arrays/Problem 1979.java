4723c35fb828c9ff37736ced683d038424ed7eb6
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-06-02 16:44:34 +0530

    Greatest Common Divisor of two numbers

--- Arrays/Problem 1979.java
+++ Arrays/Problem 1979.java
@@ -1,6 +1,5 @@
 /* Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
 The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers. */
-
 class Solution {
     public int findGCD(int[] nums) {
         int max1 = Integer.MIN_VALUE, min1 = Integer.MAX_VALUE, n;
