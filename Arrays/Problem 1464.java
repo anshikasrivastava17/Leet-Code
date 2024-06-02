2d5e35e6e698882c41c520c903fd193f3a43239e
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-06-02 16:39:39 +0530

    Max Prod of Two Elements in Array

--- Arrays/Problem 1464.java
+++ Arrays/Problem 1464.java
@@ -1,5 +1,4 @@
 /* Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1). */
-
 class Solution {
     public int maxProduct(int[] nums) {
         int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, n;
