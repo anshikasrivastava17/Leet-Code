9621fd7d28a03c34b4cee7546fa39027058c4bb3
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-06-05 17:08:31 +0530

    Fair Candy Swap

---
+++ Binary Search/Problem 888.java
@@ -0,0 +1,30 @@
+class Solution { 
+//https://leetcode.com/problems/fair-candy-swap/solutions/5184369/easy-binary-search-in-depth-explanation/
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        Arrays.sort(bobSizes);
+        int aliceSum=0;
+        int bobSum=0;
+        for(int i=0;i<aliceSizes.length;i++){
+            aliceSum+=aliceSizes[i];
+        }
+        for(int i=0;i<bobSizes.length;i++){
+            bobSum+=bobSizes[i];
+        }
+        int [] ans=new int [2];
+
+        for(int i=0;i<aliceSizes.length;i++){
+            if((bobSum-aliceSum+(2*aliceSizes[i]))%2!=0){
+                break;
+            }
+            if(Arrays.binarySearch(bobSizes,(bobSum-aliceSum+(2*aliceSizes[i]))/2)>=0){
+                ans[0]=aliceSizes[i];
+                ans[1]=(bobSum-aliceSum+(2*aliceSizes[i]))/2;
+            }
+            else if(aliceSizes[i]==(bobSum-aliceSum+(2*aliceSizes[i]))/2){
+                ans[0]=aliceSizes[i];
+                ans[1]=aliceSizes[i];
+            }
+        }
+        return ans;
+    }
+}
