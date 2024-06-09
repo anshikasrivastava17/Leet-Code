b9ed03f95dd5e38c95bd0429b0772ccacfae2df5
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-06-09 11:59:40 +0530

    Can Make Arithmetic Progression From Sequence

---
+++ Sorting/Problem 1502.java
@@ -0,0 +1,18 @@
+/* A sequence of numbers is called an arithmetic progression if the difference between any two consecutive elements is the same.
+Given an array of numbers arr, return true if the array can be rearranged to form an arithmetic progression. Otherwise, return false. */
+  
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        int diff=arr[1]-arr[0];
+        boolean temp=true;
+        for(int i=1;i<arr.length-1;i++){
+            if(arr[i+1]-arr[i]==diff)
+                continue;
+            else
+                temp=false;
+        }
+        if(temp==true) return true;
+        else return false;   
+    }
+}
