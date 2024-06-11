556a1d16684804198b803dd5c82cd7170241ba06
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-06-11 15:27:29 +0530

    Orderly Queue

---
+++ Strings/Problem 899.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String orderlyQueue(String s, int k) {
+       if(k>1){
+            char tempArray[] = s.toCharArray();
+            Arrays.sort(tempArray);
+            return new String(tempArray);
+        } 
+        else {
+            String ans  = s;
+            for(int i=0;i< s.length(); i++){
+                s = s.substring(1) + s.charAt(0);
+                if(ans.compareTo(s) >0){
+                    ans = s;
+                }
+            }
+            return ans;
+        }
+   }
+}
