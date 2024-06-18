d25fc6bb7a34a601ce8422de31b2910f709b4910
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-06-18 23:17:24 +0530

    Palindrome Number

--- Problem 9.cpp
+++ Problem 9.cpp
@@ -1,15 +1,15 @@
 /*Given an integer x, return true if x is a palindrome, and false otherwise. */
 
 class Solution {
-public:
-    bool isPalindrome(int n) {
-     if(n<0)
-     return false;
-     int k=n,rev=0;
+    public boolean isPalindrome(int n) {
+         if(n<0)
+        return false;
+
+    int k=n,rev=0;
      while(n!=0)
      {
     int r=n%10;
-    if(rev>INT_MAX/10 || rev<INT_MIN/10)
+    if(rev>Integer.MAX_VALUE/10 || rev<Integer.MIN_VALUE/10)
     return false;
      rev=(rev*10)+r;
      n/=10;
@@ -19,4 +19,4 @@ public:
      else
      return false;  
     }
-};
+}
