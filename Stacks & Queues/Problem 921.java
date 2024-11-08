09d5a1076c15f90b8f1da3d816281584bf966d8f
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-11-08 14:02:48 +0530

    Minimum Add to make Parentheses Valid

---
+++ Stacks & Queues/Problem 921.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') 
+                stack.push(ch);
+            else if (ch == ')') {
+                if (!stack.isEmpty() && stack.peek() == '(') 
+                    stack.pop();
+                else 
+                    stack.push(ch);
+            }
+        }
+        return stack.size();
+    }
+}
